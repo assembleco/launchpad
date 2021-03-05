a697df4b285a3a60cc56b340d736269b641ef01a
grace <grace.c.youngblood@gmail.com>
2021-03-05 15:02:51 -0500

    Change `app/javascript/` -> `src/`, basic homepage

--- src/packs/application.js
+++ src/packs/application.js
@@ -8,6 +8,21 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+const Hello = props => (
+  <div>Hello {props.name}!</div>
+)
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="Hierarch" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
