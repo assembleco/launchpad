808b2f08d14f21359ca4a65c2548bdfe1efc5ba4
grace <grace.c.youngblood@gmail.com>
2021-03-05 17:56:27 -0500

    [repair] blur broken code

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,15 +1,15 @@
 class LaunchesController < ApplicationController
   def new
-    channels = params.permit(:hierarch_channel, :package_channel).values
-    container = Docker::Container.create(
-      'Image' => 'assemble/hierarch',
-      'ExposedPorts' => channels.values.map{|x| ["#{x}/tcp", {}]}.to_h,
-      'HostConfig' => {
-        'PortBindings' =>
-          channels.values.map{|x| ["#{x}/tcp", [{ 'HostPort' => x }]] }.to_h
-      }
-    )
-    container.exec(['./my_service'], detach: true)
+    # channels = params.permit(:hierarch_channel, :package_channel).values
+    # container = Docker::Container.create(
+    #   'Image' => 'assemble/hierarch',
+    #   'ExposedPorts' => channels.values.map{|x| ["#{x}/tcp", {}]}.to_h,
+    #   'HostConfig' => {
+    #     'PortBindings' =>
+    #       channels.values.map{|x| ["#{x}/tcp", [{ 'HostPort' => x }]] }.to_h
+    #   }
+    # )
+    # container.exec(['./my_service'], detach: true)
 
     render json: params.
       permit(:domain, :package_channel, :hierarch_channel).
