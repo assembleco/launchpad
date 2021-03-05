07ea7d99ed7eac3e3b8fd37a4e70b2bba37b7896
grace <grace.c.youngblood@gmail.com>
2021-03-05 18:13:33 -0500

    Begin using Docker.

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,18 +1,38 @@
+require 'docker'
+
 class LaunchesController < ApplicationController
   def new
-    # channels = params.permit(:hierarch_channel, :package_channel).values
-    # container = Docker::Container.create(
-    #   'Image' => 'assemble/hierarch',
-    #   'ExposedPorts' => channels.values.map{|x| ["#{x}/tcp", {}]}.to_h,
-    #   'HostConfig' => {
-    #     'PortBindings' =>
-    #       channels.values.map{|x| ["#{x}/tcp", [{ 'HostPort' => x }]] }.to_h
-    #   }
-    # )
-    # container.exec(['./my_service'], detach: true)
+    p = params.permit(:domain, :package_channel, :hierarch_channel)
+    channels = p.slice(:hierarch_channel, :package_channel)
 
-    render json: params.
-      permit(:domain, :package_channel, :hierarch_channel).
-      merge({ launched: "no" })
+    container = Docker::Container.create(
+      'Cmd' => ['yarn', 'go'],
+      'Image' => 'assemble/hierarch',
+      'Env' => [
+        "DOMAIN=#{p[:domain]}",
+        "HIERARCH_CHANNEL=#{p[:hierarch_channel]}",
+        "PACKAGE_CHANNEL=#{p[:package_channel]}",
+      ],
+      'ExposedPorts' => channels.values.map{|x| ["#{x}/tcp", {}]}.to_h,
+      'HostConfig' => {
+        'PortBindings' =>
+        channels.values.map{|x| ["#{x}/tcp", [{ 'HostPort' => x.to_s }]] }.to_h
+      }
+    )
+    container.start
+    puts 'running'
+
+    sleep 10
+    response = []
+
+    response << container.logs(stdout: true)
+    puts 'logs:'
+    puts response[-1]
+
+    response << container.exec(['node', 'hierarch'], detach: true)
+    puts 'logs:'
+    puts response[-1]
+
+    render json: p.merge({ launched: "yes", response: response })
   end
 end
