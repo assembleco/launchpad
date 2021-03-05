61d039b655ee8e722536dea3fe4f30200092e1b8
grace <grace.c.youngblood@gmail.com>
2021-03-05 17:54:17 -0500

    [broken] bring in `docker-api` sample code.

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,5 +1,16 @@
 class LaunchesController < ApplicationController
   def new
+    container = Docker::Container.create(
+      'Image' => 'assemble/hierarch',
+      'ExposedPorts' => { '1234/tcp' => {} },
+      'HostConfig' => {
+        'PortBindings' => {
+          '1234/tcp' => [{ 'HostPort' => '1234' }]
+        }
+      }
+    )
+    container.exec(['./my_service'], detach: true)
+
     render json: params.
       permit(:domain, :package_channel, :hierarch_channel).
       merge({ launched: "no" })
