e81df071ad8d14cf2b961331f8d369dbce821435
grace <grace.c.youngblood@gmail.com>
2021-03-05 16:03:38 -0500

    [broken] params are broken, none come through.

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,9 +1,16 @@
 class LaunchesController < ApplicationController
   def new
-    render json: params.permit(
+    vars = params.permit(
       :domain,
       :package_channel,
       :hierarch_channel,
-    ).to_h.merge({ launched: "no" })
+    ).slice(
+      :domain,
+      :package_channel,
+      :hierarch_channel,
+    ).merge({ launched: "no" })
+
+    puts vars
+    render json: vars
   end
 end
