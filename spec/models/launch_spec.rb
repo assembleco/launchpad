46bf9755af98774f06357d8080da2c61b1eced85
grace <grace.c.youngblood@gmail.com>
2021-03-14 20:29:34 -0400

    Add some logic in on Launch spec.

--- spec/models/launch_spec.rb
+++ spec/models/launch_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Launch do
   describe ".go" do
-    it "chooses a couple open channels" do
+    it "chooses a couple open channels and re-uses closed ones" do
       launches = [
         Launch.go("192.168.1.1", package_channel: 1024, hierarch_channel: 1025),
         Launch.go("192.168.1.2", package_channel: 1026, hierarch_channel: 1027),
@@ -13,6 +13,13 @@ RSpec.describe Launch do
       expect(launch.package_channel).to eq 1030
       expect(launch.hierarch_channel).to eq 1031
       expect(launch.user_address).to eq "192.168.1.4"
+
+      launches.each { |l| l.land }
+
+      second_launch = Launch.go("192.168.1.5")
+
+      expect(second_launch.package_channel).to eq 1024
+      expect(second_launch.hierarch_channel).to eq 1025
     end
   end
 end
