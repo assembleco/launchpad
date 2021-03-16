4145bce95419f6911f5594c95f3560668df3fa5d
grace <grace.c.youngblood@gmail.com>
2021-03-16 16:37:25 -0400

    Add `.env`'s `DOMAIN` in our config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  domain = ENV.fetch("DOMAIN") rescue nil
+  config.hosts << domain if domain
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
