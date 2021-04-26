2035d0342f8a3daecaba5fca9ec4a4f10f3309a5
grace <grace.c.youngblood@gmail.com>
2021-04-26 12:54:54 -0400

    Small db extension change.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_16_163245) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "launches", force: :cascade do |t|
     t.string "user_address"
     t.integer "package_channel"
